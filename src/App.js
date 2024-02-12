36014ba7e5dce940a36365c8a75d7a55f197137b
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2024-02-12 13:29:25 -0500

    List has been create for add the src using handlekeypress for key press

--- src/App.js
+++ src/App.js
@@ -1,136 +1,59 @@
-
-import React from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import './App.css';
-import { useState, useEffect, useRef} from 'react';
+import { audioList } from './audioBank.js';
 
 function App() {
-  
-  const [audioSrc, setAudioSrc] = useState('');
-  const [displayText, setDisplayText] =useState('');
+  const [displayText, setDisplayText] = useState('');
   const audioRef = useRef('');
-  const volumeRef = useRef(); 
+  const volumeRef = useRef(0);
+  const [audioId, setAudioID] = useState('');
+  const [currentAudio, setCurrentAudio] = useState(null);
 
-  useEffect(() => { 
-  document.addEventListener("keydown", function(event){
-    switch(event.key){
-      case "q":
-        return keypressq()
-      case "w":
-        return keypressw()
-      case "e":
-        return keypresse()
-      case "a":
-        return keypressa()  
-      case "s":
-        return keypresss()  
-      case "d":
-          return keypressd()
-      case "z":
-        return keypressz()      
-      case "x":
-        return keypressx()      
-      case "c":
-        return keypressc()   
-      default:
+  const handleKeyPress = useCallback((pressedKey) => {
+    const audio = audioList.find((audio) => audio.key === pressedKey.toLowerCase());
+    if (audio) {
+      const audioElement = new Audio(audio.src);
+      setAudioID(audio.id);
+      audioElement.play();
+      setCurrentAudio(audio);
+      setDisplayText(audio.name);
     }
-  });
-},[]);
+  }, []);
 
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      handleKeyPress(event.key);
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyPress]);
 
-
-  function keypressq() {
-    const newAudioSrc = 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3';
-    setAudioSrc(newAudioSrc);
-    audioRef.current.play();
-    setDisplayText("Heater 1")
-  
-  }
-
-  function keypressw() {
-    const newAudioSrc = 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3';
-    setAudioSrc(newAudioSrc);
-    audioRef.current.play();
-    setDisplayText("Heater 2")
-   
-  }
-  function keypresse() {
-    const newAudioSrc = 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3';
-    setAudioSrc(newAudioSrc);
-    audioRef.current.play();
-    setDisplayText("Heater 3")
-   
-  }
-  function keypressa() {
-    const newAudioSrc = 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3';
-    setAudioSrc(newAudioSrc);
-    audioRef.current.play();
-    setDisplayText("Heater 4")
-   
-  }
-  function keypresss() {
-    const newAudioSrc = 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3';
-    setAudioSrc(newAudioSrc);
-    audioRef.current.play();
-    setDisplayText("Clap")
-   
-  }
-  function keypressd() {
-    const newAudioSrc = 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3';
-    setAudioSrc(newAudioSrc);
-    audioRef.current.play();
-    setDisplayText("Clap")
-   
-  }
-
-  function keypressz() {
-    const newAudioSrc = 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3';
-    setAudioSrc(newAudioSrc);
-    audioRef.current.play();
-    setDisplayText("Clap")
-   
-  }
-  function keypressx() {
-    const newAudioSrc = 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3';
-    setAudioSrc(newAudioSrc);
-    audioRef.current.play();
-    setDisplayText("Clap")
-   
-  }
-
-  function keypressc() {
-    const newAudioSrc = 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3';
-    setAudioSrc(newAudioSrc);
-    audioRef.current.play();
-    setDisplayText("Clap")
-   
-  }
   const handleVolumeChange = (e) => {
     const newVolume = e.target.value / 100;
     volumeRef.current = newVolume;
     audioRef.current.volume = newVolume;
   };
-
   
+  console.log(audioId);
   return (
     <div>
       <div id="drum-machine">
         <div className="buttons_container">
-        <button className="drum-pad" id="Q" type="button" onClick={() => {keypressq()}}>Q</button>
-        <button className="drum-pad" id="W" type="button" onClick={() => {keypressw()}}>W</button>
-        <button className="drum-pad" id="E" type="button" onClick={() => {keypressq()}}>E</button>
-        <button className="drum-pad" id="A" type="button" onClick={() => {keypressq()}}>A</button>
-        <button className="drum-pad" id="S" type="button" onClick={() => {keypressq()}}>S</button>
-        <button className="drum-pad" id="D" type="button" onClick={() => {keypressq()}}>D</button>
-        <button className="drum-pad" id="Z" type="button" onClick={() => {keypressq()}}>Z</button>
-        <button className="drum-pad" id="X" type="button" onClick={() => {keypressq()}}>X</button>
-        <button className="drum-pad" id="C" type="button" onClick={() => {keypressq()}}>C</button>
+          {audioList.map((audio, index) => (
+            <div key={index}>
+              <button className="drum-pad" id={audio.id} type="button" onClick={() => handleKeyPress(audio.key)}>{audio.id}
+              </button>
+               <audio className="clip" id={audio.id} src={currentAudio}  />
+            </div>
+          ))}
         </div>
         <div id="display">
           <label className='switchBtn'>
-          <input type="checkbox" />
-          <div className="slide" >On Off </div>
+            <input type="checkbox" />
+            <div className="slide" >On Off </div>
           </label>
-       
           <h2>{displayText}</h2>
           <input
             className="volume-range"
@@ -140,13 +63,9 @@ function App() {
             onChange={handleVolumeChange}
           />
         </div>
- 
-      
-   
-      <audio  className="clip" ref={audioRef} src={audioSrc} id={displayText}/>
+       
       </div>
-      </div>
-   
+    </div>
   );
 }
 
---
+++ src/audioBank.js
@@ -0,0 +1,13 @@
+const audioList =[
+    {key : 'q', id:'Q', name:'Heater 1' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
+    {key : 'w', id:'W', name:'Heater 2' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
+    {key : 'e', id:'E', name:'Heater 3' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
+    {key : 'a', id:'A', name:'Heater 4' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
+    {key : 's', id:'S', name:'Clap' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
+    {key : 'd', id:'D', name:'Open-HH' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
+    {key : 'z', id:'Z', name:"Kick-n'-Hat" , src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
+    {key : 'x', id:'X', name:'Kick' , src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
+    {key : 'c', id:'C', name:'Closed-HH' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' },
+    
+  ];
+export {audioList};
