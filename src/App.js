b74c38840dd3939448e0eba958cf7ab0506a907b
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2024-02-27 09:11:23 -0500

    las update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import './App.css';
 import { audioList } from './audioBank.js';
 
 function App() {
   const [displayText, setDisplayText] = useState('');
-  const audioRef = useRef('');
-  const volumeRef = useRef(0);
-  const [audioId, setAudioID] = useState('');
-  const [currentAudio, setCurrentAudio] = useState(null);
+  const audioRef = useRef(new Audio());
 
   const handleKeyPress = useCallback((pressedKey) => {
     const audio = audioList.find((audio) => audio.key === pressedKey.toLowerCase());
     if (audio) {
-      const audioElement = new Audio(audio.src);
-      setAudioID(audio.id);
-      audioElement.play();
-      setCurrentAudio(audio);
+      audioRef.current.src = audio.src;
+      audioRef.current.play();
       setDisplayText(audio.name);
     }
   }, []);
@@ -31,46 +26,30 @@ function App() {
   }, [handleKeyPress]);
 
   const handleVolumeChange = (e) => {
-    const newVolume = e.target.value / 100;
-    volumeRef.current = newVolume;
-    audioRef.current.volume = newVolume;
+    audioRef.current.volume = e.target.value / 100;
   };
   
-  console.log(audioId);
+  
   return (
-    <>
-    <div>
-      <div id="drum-machine">
-        <div className="buttons_container">
-          {audioList.map((audio, index) => (
-            <div key={index}>
-              <div className="drum-pad" id={audio.id} onClick={() => handleKeyPress(audio.key)}>{audio.id}
-              <audio className="clip" id={audio.id} src={audio.src}  />
-              </div>
-               
-            </div>
-           
-          )
-          )}
-        </div>
-        <div id="display">
-          <label className='switchBtn'>
-            <input type="checkbox" />
-            <div className="slide" >On Off </div>
-          </label>
-          <h2>{displayText}</h2>
-          <input
-            className="volume-range"
-            type="range"
-            min="0"
-            max="100"
-            onChange={handleVolumeChange}
-          />
-        </div>
-       
+    <div id="drum-machine">
+      <div className="buttons_container">
+        {audioList.map((audio, index) => (
+          <div key={index} className="drum-pad" id={audio.id} onClick={() => handleKeyPress(audio.key)}>
+            {audio.id}
+            <audio className="clip" id={audio.id} src={audio.src}/>
+          </div>
+        ))}
+      </div>
+      <div id="display">
+        <label className='switchBtn'>
+          <input type="checkbox" />
+          <div className="slide" >On Off </div>
+        </label>
+        <h2>{displayText}</h2>
+        <input className="volume-range" type="range" min="0" max="100" onChange={handleVolumeChange} />
       </div>
     </div>
-    </>);
+  );
 }
 
 export default App;
