1889f316f5de15273d3dfa2340d12b7e68f5d555
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2024-02-28 10:14:14 -0500

    update last part

--- src/App.js
+++ src/App.js
@@ -1,55 +1,71 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React from 'react';
+//import React, {useEffect } from 'react';
 import './App.css';
-import { audioList } from './audioBank.js';
+import {DrumPads} from './audioBank.js'; 
 
-function App() {
-  const [displayText, setDisplayText] = useState('');
-  const audioRef = useRef(new Audio());
-
-  const handleKeyPress = useCallback((pressedKey) => {
-    const audio = audioList.find((audio) => audio.key === pressedKey.toLowerCase());
-    if (audio) {
-      audioRef.current.src = audio.src;
-      audioRef.current.play();
-      setDisplayText(audio.name);
-    }
-  }, []);
-
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      handleKeyPress(event.key);
-    };
-    window.addEventListener('keydown', handleKeyDown);
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [handleKeyPress]);
-
-  const handleVolumeChange = (e) => {
-    audioRef.current.volume = e.target.value / 100;
-  };
-  
-  
+const DrumPad = ({ drumKey, song, handleClick, url }) => {
   return (
-    <div id="drum-machine">
-      <div className="buttons_container">
-        {audioList.map((audio, index) => (
-          <div key={index} className="drum-pad" id={audio.id} onClick={() => handleKeyPress(audio.key)}>
-            {audio.id}
-            <audio className="clip" id={audio.id} src={audio.src}/>
-          </div>
-        ))}
-      </div>
-      <div id="display">
-        <label className='switchBtn'>
-          <input type="checkbox" />
-          <div className="slide" >On Off </div>
-        </label>
-        <h2>{displayText}</h2>
-        <input className="volume-range" type="range" min="0" max="100" onChange={handleVolumeChange} />
-      </div>
-    </div>
+      <button className="drum-pad" id={song} onClick={() => handleClick(drumKey,song)}>
+          {drumKey}
+          <audio className="clip" src={url} id={drumKey}/>
+      </button>
   );
-}
+};
+
+const DrumMachine = () => {
+  
+  const [currentSongText, setCurrentSongText] = React.useState('');
+
+  React.useEffect(() => {
+      const handleKeyPress = (e) => {
+          const pad = DrumPads.find(
+              item => item.key === e.key.toUpperCase(),
+          );
+          if (pad) document.getElementById(pad.song).click();
+      };
+
+      window.addEventListener('keypress', handleKeyPress);
+
+      return () => {
+          window.removeEventListener('keypress', handleKeyPress);
+      };
+  }, );
+ 
+  const handleButtonClick = (key, song) => {
+      document.getElementById(key).play();
+      setCurrentSongText(song);
+  };
+
+  return (
+      <div id="drum-machine">
+          <div className="buttons_container">
+              <div id="display-pads" >
+                  {DrumPads.map(item => (
+                      <DrumPad 
+                          song={item.song}
+                          key={item.key}
+                          drumKey={item.key}
+                          handleClick={handleButtonClick}
+                          url={item.url}
+                          className='drum-pad' />
+                  ))}
+              </div>
+              
+          </div>
+          <div className="app_title">
+              <h1>Drum Machine App</h1>
+          </div>
+          <p id="display" className="current-text">{currentSongText}</p>
+      </div>
+  );
+};
+
+const App = () => {
+  return (
+      <div className="App">
+          <DrumMachine/>
+      </div>
+  );
+};
 
 export default App;
--- src/audioBank.js
+++ src/audioBank.js
@@ -1,13 +1,47 @@
-const audioList =[
-    {key : 'q', id:'Q', name:'Heater 1' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
-    {key : 'w', id:'W', name:'Heater 2' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
-    {key : 'e', id:'E', name:'Heater 3' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
-    {key : 'a', id:'A', name:'Heater 4' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
-    {key : 's', id:'S', name:'Clap' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
-    {key : 'd', id:'D', name:'Open-HH' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
-    {key : 'z', id:'Z', name:"Kick-n'-Hat" , src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
-    {key : 'x', id:'X', name:'Kick' , src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
-    {key : 'c', id:'C', name:'Closed-HH' , src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' },
-    
-  ];
-export {audioList};
+const DrumPads =  [{
+  "key": "Q",
+  "song": "Heater-1",
+  "url": "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+},
+{
+  "key": "W",
+  "song": "Heater-2",
+  "url": "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+},
+{
+  "key": "E",
+  "song": "Heater-3",
+  "url": "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+},
+{
+  "key": "A",
+  "song": "Heater-4",
+  "url": "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+},
+{
+  "key": "S",
+  "song": "Heater-6",
+  "url": "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+},
+{
+  "key": "D",
+  "song": "Dsc_Oh",
+  "url": "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+},
+{
+  "key": "Z",
+  "song": "Kick_n_Hat",
+  "url": "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+},
+{
+  "key": "X",
+  "song": "RP4_KICK_1",
+  "url": "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+},
+{
+  "key": "C",
+  "song": "Cev_H2",
+  "url": "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+}
+];
+export {DrumPads};
